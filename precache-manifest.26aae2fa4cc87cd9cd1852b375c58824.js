b0bf0a17fb3ab73096092452efdb06b7ba0e77f0
Ben Grawe <bgrawe@jcp.com>
2019-07-15 11:25:40 -0500

    Updates

---
+++ precache-manifest.26aae2fa4cc87cd9cd1852b375c58824.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a1cbfed927d6b6164ff48b95c04dc20",
+    "url": "/ascii-value-react/index.html"
+  },
+  {
+    "revision": "9ff7089fa647cb698c64",
+    "url": "/ascii-value-react/static/css/main.1b5cad32.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/ascii-value-react/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "9ff7089fa647cb698c64",
+    "url": "/ascii-value-react/static/js/main.ea9d61c2.chunk.js"
+  },
+  {
+    "revision": "ec9c0ce2559a1de36ec3",
+    "url": "/ascii-value-react/static/js/runtime~main.5196e199.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ascii-value-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
